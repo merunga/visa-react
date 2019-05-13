2c49a02a5c806ec8dd10922abcac363ba15d0a75
merunga <merunga@gmail.com>
2019-05-12 20:15:08 -0500

    Updates

---
+++ precache-manifest.7a269bb9474570f10eda478de5fcc1f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2570e30b345dde5d2cf833ae85de014",
+    "url": "/visa-react/index.html"
+  },
+  {
+    "revision": "4a6e749a8a24b828c5e1",
+    "url": "/visa-react/static/css/main.d7bbbd9f.chunk.css"
+  },
+  {
+    "revision": "56a6598381f5d28fe34d",
+    "url": "/visa-react/static/js/2.90b60223.chunk.js"
+  },
+  {
+    "revision": "4a6e749a8a24b828c5e1",
+    "url": "/visa-react/static/js/main.fa5e8b60.chunk.js"
+  },
+  {
+    "revision": "b7a9fc7acfb917930e84",
+    "url": "/visa-react/static/js/runtime~main.0f3802e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.486598a9d74cc5e6497cd6aeb16c1a9a.js"
+  "/visa-react/precache-manifest.7a269bb9474570f10eda478de5fcc1f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/visa-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
