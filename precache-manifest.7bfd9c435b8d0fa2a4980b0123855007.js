b795d77d09670dd6ccca86c332b3225687491d5c
merunga <merunga@gmail.com>
2019-05-12 20:20:08 -0500

    Updates

--- precache-manifest.7bfd9c435b8d0fa2a4980b0123855007.js
+++ precache-manifest.7bfd9c435b8d0fa2a4980b0123855007.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd8e6b566cc0134a4b458c9dc4c12141",
+    "revision": "1789af24cdbd650b29c62d01d4215034",
     "url": "/visa-react/index.html"
   },
   {
-    "revision": "fc244c4004c11b215e8f",
+    "revision": "efbc03ce06b346ceca9d",
     "url": "/visa-react/static/css/main.d7bbbd9f.chunk.css"
   },
   {
-    "revision": "62399ed5a142880cf16a",
-    "url": "/visa-react/static/js/2.b1062137.chunk.js"
+    "revision": "9f9fd992554164ea9322",
+    "url": "/visa-react/static/js/2.4eeb3ede.chunk.js"
   },
   {
-    "revision": "fc244c4004c11b215e8f",
-    "url": "/visa-react/static/js/main.d97861b5.chunk.js"
+    "revision": "efbc03ce06b346ceca9d",
+    "url": "/visa-react/static/js/main.5f10b6f4.chunk.js"
   },
   {
     "revision": "b7a9fc7acfb917930e84",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/visa-react/precache-manifest.3bb61266b43cc50b5a745b49a97fedf0.js"
+  "/visa-react/precache-manifest.7bfd9c435b8d0fa2a4980b0123855007.js"
 );
 
 self.addEventListener('message', (event) => {
